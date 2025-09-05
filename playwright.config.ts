0969629d0e6572787992ddf2da5e374aaf3c342d
QA Student Morning <qastudentmorning@Mac-mini.local>
2025-09-05 09:26:33 -0600

    fix actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,10 +23,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ["html", { open: 'never', outputFolder: 'end2end-report' }],
+    ["html", { open: "never", outputFolder: "end2end-report" }],
     [
-      "playwright-ctrf-json-reporter", { outputDir: "end2end", outputFile: "ci-nightly-test.json" }
-    ]],
+      "playwright-ctrf-json-reporter",
+      { outputDir: "end2end", outputFile: "ci-nighly-test.json" },
+    ]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
